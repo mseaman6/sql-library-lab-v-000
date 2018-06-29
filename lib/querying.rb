8e5264a875233b3708255bf5f9f437bd976ff2c6
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-29 01:26:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species, COUNT(characters.species) AS character_species_count FROM series JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human' GROUP BY series.title, characters.species ORDER BY character_species_count DESC LIMIT 1;"
+  "SELECT series.title, characters.species, COUNT(characters.species) FROM series JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human' GROUP BY series.title, characters.species ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
