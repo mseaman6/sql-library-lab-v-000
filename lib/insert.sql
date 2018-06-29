1a6f899aa5d70902e388de233a36dbbeea7238f2
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-29 00:01:53 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ();
-INSERT INTO series (title, author_id, subgenre_id) VALUES ();
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Game of Thrones", );
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", );
 
 INSERT INTO books (title, year, series_id) VALUES ();
 INSERT INTO books (title, year, series_id) VALUES ();
@@ -20,8 +20,8 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ();
 INSERT INTO subgenres (name) VALUES ();
 INSERT INTO subgenres (name) VALUES ();
 
-INSERT INTO authors (name) VALUES ();
-INSERT INTO authors (name) VALUES ();
+INSERT INTO authors (name) VALUES ("George RR Martin");
+INSERT INTO authors (name) VALUES ("J.R. Tolkien");
 
 INSERT INTO character_books (character_id, book_id) VALUES ();
 INSERT INTO character_books (character_id, book_id) VALUES ();
