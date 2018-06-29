4d4af3fa0858e84739be4b2a8232219ad2dfa718
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-29 01:02:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species, SUM(characters.species) FROM characters JOIN series ON characters.series_id = series.id GROUP BY series.title, characters.species;"
+  "SELECT series.title, characters.species, SUM(characters.species) FROM series JOIN characters ON series.id = characters.series_id GROUP BY series.title, characters.species;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
